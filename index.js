9c1b1a2f9d02489aee0805020f3768a081579d81
sanarguz <santiago.arcilag@gmail.com>
2020-07-12 23:52:28 -0500

    palindromo

---
+++ index.js
@@ -0,0 +1,23 @@
+function palindromo() {
+    const A = Array.from(document.getElementById("cadena").value.toLowerCase())
+    const B = A.slice()
+    A.reverse()
+    
+    if(compareArray(A,B)){
+        document.getElementById("result-container").className = "alert alert-success mt-5"
+        document.getElementById("result").innerHTML = "Es Palindromo "
+    } else {
+        document.getElementById("result-container").className = "alert alert-danger mt-5"
+        document.getElementById("result").innerHTML = "No es Palindromo "
+    }
+}
+
+function compareArray(a, b){
+    for (i in a) {
+        if (a[i] != b[i]) {
+            return false
+        } else {
+            return true
+        }
+    }
+}
